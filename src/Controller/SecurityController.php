8957a77a47e1c05809ef9a4228f2e3404c990ffa
dr-matt-smith <matt.smith@itb.ie>
2019-02-25 21:52:45 +0000

    working custom login form

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils)
+    {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        $template = 'security/login.html.twig';
+        $args = [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ];
+
+        return $this->render($template, $args);
+    }
+}
